b0351e6518ddd660aa2a7abff1a8770b9ec6a1e0
remold <remold@everett.nl>
2013-09-10 14:50:03 +0200

    Add oauth check to CheckNameEn

--- src/SURFnet/janus/validate/validators/CheckNameEn.php
+++ src/SURFnet/janus/validate/validators/CheckNameEn.php
@@ -31,6 +31,13 @@ class CheckNameEn extends Validate implements ValidateInterface
         }
     }
 
+    public function oauth(array $entityData, array $metadata, array $allowedEntities, array $blockedEntities, $arp)
+    {
+        if (!isset($metadata['name']['en'])) {
+            $this->logWarn("no english name set");
+        }
+    }
+
     public function idp(array $entityData, array $metadata, array $allowedEntities, array $blockedEntities, array $disableConsent)
     {
         if (!isset($metadata['name']['en'])) {

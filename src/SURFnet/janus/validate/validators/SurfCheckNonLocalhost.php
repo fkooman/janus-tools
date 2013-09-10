2bb458357ef4f1f296fdf2fd40b573746a4facd4
remold <remold@everett.nl>
2013-09-10 16:11:27 +0200

    add SurfCheckNonLocalhost

---
+++ src/SURFnet/janus/validate/validators/SurfCheckNonLocalhost.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright 2013 Remold Krol <remold.krol@everett.nl>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+namespace SURFnet\janus\validate\validators;
+
+use SURFnet\janus\validate\Validate;
+use SURFnet\janus\validate\ValidateInterface;
+
+class SurfCheckNonLocalhost extends Validate implements ValidateInterface
+{
+
+    /**
+     * @param array $entityData
+     * @param array $metadata
+     * @param array $allowedEntities
+     * @param array $blockedEntities
+     * @param $arp
+     */
+    public function sp(
+        array $entityData,
+        array $metadata,
+        array $allowedEntities,
+        array $blockedEntities,
+        $arp
+    ) {
+        $this->_checkEntityIdNotLocalhost($entityData);
+    }
+
+    /**
+     * @param array $entityData
+     * @param array $metadata
+     * @param array $allowedEntities
+     * @param array $blockedEntities
+     * @param array $disableConsent
+     */
+    public function idp(
+        array $entityData,
+        array $metadata,
+        array $allowedEntities,
+        array $blockedEntities,
+        array $disableConsent
+    ) {
+        $this->_checkEntityIdNotLocalhost($entityData);
+    }
+
+    /**
+     * @param array $entityData
+     */
+    private function _checkEntityIdNotLocalhost(array $entityData)
+    {
+        if (isset($entityData['entityid'])) {
+            if (strpos($entityData['entityid'], 'localhost') !== false) {
+                $this->logWarn("EntityID contains 'localhost'");
+            }
+        }
+    }
+}
